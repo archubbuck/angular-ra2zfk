3d22b75922cdd1a8e89f18e6c07c6caf9c590980
archubbuck <adam.chubbuck@gmail.com>
2023-10-01 15:26:23 -0400

    3 files modified

--- src/app/model.ts
+++ src/app/model.ts
@@ -2,3 +2,33 @@ export class DataSetColumn {
   public id: string;
   public displayName?: string = '';
 }
+
+export type ColumnIdentifier =
+  | 'issuerName'
+  | 'recommendation'
+  | 'relativeValue'
+  | 'outlook';
+
+export type DataSetColumn = {
+  id: ColumnIdentifier;
+  displayName?: string;
+};
+
+export const columns: Array<DataSetColumn> = [
+  {
+    id: 'issuerName',
+    displayName: 'Issuer Name',
+  },
+  {
+    id: 'recommendation',
+    displayName: 'Recommendation',
+  },
+  {
+    id: 'relativeValue',
+    displayName: 'Relative Value',
+  },
+  {
+    id: 'outlook',
+    displayName: 'Outlook',
+  },
+];
